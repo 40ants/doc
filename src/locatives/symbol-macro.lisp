70cce8c07850e5cb79e6843ec2e2f73da606b81b
rain3 <1487165597@qq.com>
2021-10-14 17:10:02 +0300

    Fixed locatives/symbol-macro.lisp

--- src/locatives/symbol-macro.lisp
+++ src/locatives/symbol-macro.lisp
@@ -2,27 +2,26 @@
   (:use #:cl)
   (:import-from #:40ants-doc/locatives/base
                 #:locate-and-find-source
-                #:locate-and-document
                 #:locate-error
                 #:locate-object
                 #:define-locative-type)
-  (:import-from #:40ants-doc/document
-                #:document-object)
   (:import-from #:40ants-doc/render/args)
-  (:import-from #:40ants-doc/builder/bullet)
+  (:import-from #:40ants-doc/reference-api
+                #:canonical-reference)
   (:import-from #:40ants-doc/args)
   (:import-from #:40ants-doc/reference)
   (:import-from #:40ants-doc/builder/vars)
-  (:import-from #:40ants-doc/render/print)
   (:import-from #:40ants-doc/utils)
   (:import-from #:40ants-doc/page)
   (:import-from #:40ants-doc/builder/printer)
-  (:import-from #:40ants-doc/markdown/transform)
   (:import-from #:swank-backend)
   (:import-from #:40ants-doc/source-api)
   (:import-from #:40ants-doc/locatives
                 #:symbol-macro)
-  (:import-from #:40ants-doc/locatives/utils))
+  (:import-from #:40ants-doc/locatives/utils)
+  (:import-from #:40ants-doc/commondoc/builder)
+  (:import-from #:40ants-doc/commondoc/bullet)
+  (:import-from #:40ants-doc/docstring))
 
 (in-package 40ants-doc/locatives/symbol-macro)
 
@@ -37,14 +36,18 @@
     (locate-error))
   (40ants-doc/reference::make-reference symbol (cons locative-type locative-args)))
 
-(defmethod locate-and-document ((symbol symbol)
-                                (locative-type (eql 'symbol-macro))
-                                locative-args stream)
-  (40ants-doc/builder/bullet::locate-and-print-bullet locative-type locative-args symbol stream)
-  (40ants-doc/builder/bullet::print-end-bullet stream)
-  (40ants-doc/args::with-dislocated-symbols ((list symbol))
-    (40ants-doc/render/print::maybe-print-docstring symbol :symbol-macro stream)))
+(defmethod 40ants-doc/commondoc/builder::reference-to-commondoc ((symbol symbol) (locative-type (eql 'symbol-macro)) locative-args)
+  (let* ((reference (canonical-reference
+                     (40ants-doc/reference::make-reference
+                      symbol (cons locative-type locative-args))))
+         (docstring (40ants-doc/docstring:get-docstring symbol :symbol-macro))
+         (children (when docstring
+                     (40ants-doc/commondoc/markdown:parse-markdown docstring))))
 
+    (40ants-doc/commondoc/bullet::make-bullet reference
+                                              :arglist locative-args
+                                              :children children
+                                              :ignore-words symbol)))
 
 (defmethod locate-and-find-source (symbol
                                    (locative-type (eql 'symbol-macro))
@@ -57,3 +60,4 @@
         (40ants-doc/locatives/utils::find-one-location (swank-backend:find-definitions symbol)
                                                        '("function" "operator"))
         location)))
+
