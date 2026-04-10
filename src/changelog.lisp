ad5b47ef926504a7b6c4143b82a0548cc201f434
Alexander Artemenko <svetlyak.40wt@gmail.com>
2026-04-11 00:52:52 +0300

    Supported documentation building for projects cloned as git submodules.

--- src/changelog.lisp
+++ src/changelog.lisp
@@ -155,6 +155,8 @@
                               "CLEAN-URLS"
                               ;; These objects are not documented yet:
                               "40ANTS-DOC/COMMONDOC/XREF:XREF"))
+  (0.26.0 2026-04-11
+          "* Supported documentation building for projects cloned as git submodules.")
   (0.25.1 2025-09-13
           "* New package 40ants-doc/mito now exports `fixed-dao-table-class` class which should be used instead of Mito's `dao-table-class`.")
   (0.25.0 2025-09-13
